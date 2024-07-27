eb0b17b9b2074e92dc9a758f4740110e88ea708c
spiderjose12 <piplup4354@gmail.com>
2024-07-26 21:41:01 -0500

    Create meme.py

---
+++ meme.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "ROFL se utiliza como reacción a algo gracioso, similar a LOL"
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("esa palabra no esta en diccionario pronto lo actualizaremos")
